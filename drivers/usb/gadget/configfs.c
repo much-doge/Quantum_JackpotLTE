42ffa44e4e1be01bfc44cf7e6ce6688787d0b0d5
much-doge <bo.observer.2@gmail.com>
2022-07-02 18:27:16 +0700

    usb: gadget: configfs: fix concurrent issue between composite APIs
    Fix merge error

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -1733,6 +1733,7 @@ static void configfs_composite_unbind(struct usb_gadget *gadget)
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
 
+/*
 static int configfs_composite_setup(struct usb_gadget *gadget,
 		const struct usb_ctrlrequest *ctrl)
 {
@@ -1779,6 +1780,7 @@ static void configfs_composite_disconnect(struct usb_gadget *gadget)
 	composite_disconnect(gadget);
 	spin_unlock_irqrestore(&gi->spinlock, flags);
 }
+*/
 
 static void configfs_composite_suspend(struct usb_gadget *gadget)
 {
