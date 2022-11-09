323e292feea1be24e2ba9ecc3594f49479063f7a
much-doge <bo.observer.2@gmail.com>
2022-11-10 06:17:06 +0700

    Change Linaro to version 5.5

--- cronos.sh
+++ cronos.sh
@@ -18,11 +18,11 @@
 
 # Directory Contol
 CR_DIR=$(pwd)
-CR_TC=/home/miko/Documents/playground/linaro_-_4.9/bin/aarch64-linux-gnu-
+CR_TC=/home/miko/Documents/playground/linaro_-_5.5/bin/aarch64-linux-gnu-
 CR_DTS=arch/arm64/boot/dts
 CR_DTB=$CR_DIR/boot.img-dtb
 # Kernel Variables
-CR_VERSION=V1.1.3
+CR_VERSION=V1.1.4
 CR_NAME=Quantum_Quackery
 CR_JOBS=5
 CR_ANDROID=p
