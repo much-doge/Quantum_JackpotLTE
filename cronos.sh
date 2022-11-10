e348910e7dbe58827d5c1f38514888010baf51b7
much-doge <bo.observer.2@gmail.com>
2022-11-10 16:38:51 +0700

    Use GCC 12.2.1

--- cronos.sh
+++ cronos.sh
@@ -18,15 +18,15 @@
 
 # Directory Contol
 CR_DIR=$(pwd)
-CR_TC=/home/miko/Documents/playground/linaro_-_5.5/bin/aarch64-linux-gnu-
+CR_TC=/home/miko/Documents/playground/linaro_-_12.2.1/bin/aarch64-linux-gnu-
 CR_DTS=arch/arm64/boot/dts
 CR_DTB=$CR_DIR/boot.img-dtb
 # Kernel Variables
 CR_VERSION=V1.1.3
 CR_NAME=Quantum_Quackery
 CR_JOBS=8
-CR_ANDROID=p
-CR_PLATFORM=9
+CR_ANDROID=q
+CR_PLATFORM=10
 CR_ARCH=arm64
 CR_DATE=$(date +%Y%m%d)
 # Init build
