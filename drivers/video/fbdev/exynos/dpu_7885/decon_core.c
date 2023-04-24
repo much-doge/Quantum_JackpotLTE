7cdcfd78cb9ae04764f3ae5cdc8dfda66787ad14
much-doge <bo.observer.2@gmail.com>
2023-04-24 21:50:46 +0700

    drivers: fbdev: add a timer to the held wakelock
    This wakelock seems to be getting stuck for as long as the device is on and drains
    battery considerably. This commit adds a timeout of 500ms to the wakelock, which
    should be sufficiently enough for it do it's job.
    
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/video/fbdev/exynos/dpu_7885/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_7885/decon_core.c
@@ -517,8 +517,10 @@ static int decon_enable(struct decon_device *decon)
 	if (decon->dt.disp_freq)
 		decon->bts.ops->bts_update_qos_disp(decon, decon->dt.disp_freq);
 
-	pm_stay_awake(decon->dev);
-	dev_warn(decon->dev, "pm_stay_awake");
+	pm_wakeup_event(decon->dev, 500);
+	dev_warn(decon->dev, "wakelock held for 500ms");
+	/* pm_stay_awake(decon->dev); */
+	/* dev_warn(decon->dev, "pm_stay_awake"); */
 	ret = v4l2_subdev_call(decon->out_sd[0], video, s_stream, 1);
 	if (ret) {
 		decon_err("starting stream failed for %s\n",
@@ -666,10 +668,10 @@ static int decon_disable(struct decon_device *decon)
 	if (decon->dt.disp_freq)
 		decon->bts.ops->bts_update_qos_disp(decon, 0);
 
-	if (decon->dt.out_type == DECON_OUT_DSI) {
-		pm_relax(decon->dev);
-		dev_warn(decon->dev, "pm_relax");
-	}
+/*	if (decon->dt.out_type == DECON_OUT_DSI) { */
+/*		pm_relax(decon->dev); */
+/*		dev_warn(decon->dev, "pm_relax"); */
+/*	} */
 
 	if (decon->dt.psr_mode != DECON_VIDEO_MODE) {
 		if (decon->res.pinctrl && decon->res.hw_te_off) {
@@ -3074,8 +3076,10 @@ static int decon_probe(struct platform_device *pdev)
 			goto err_display;
 		}
 
-		pm_stay_awake(decon->dev);
-		dev_warn(decon->dev, "pm_stay_awake");
+		pm_wakeup_event(decon->dev, 500);
+		dev_warn(decon->dev, "wakelock held for 500ms");
+		/* pm_stay_awake(decon->dev); */
+		/* dev_warn(decon->dev, "pm_stay_awake"); */
 	}
 
 #ifndef CONFIG_EXYNOS_SUPPORT_FB_HANDOVER
