217a67ba4426875853deb2d17c2f10a37cafae24
much-doge <bo.observer.2@gmail.com>
2023-05-14 23:51:15 +0700

    Revert hacky changes in net: tcp_input

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4758,7 +4758,7 @@ static void tcp_sack_remove(struct tcp_sock *tp)
 
 			/* Zap this SACK, by moving forward any other SACKS. */
 			for (i = this_sack+1; i < num_sacks; i++)
-				tp->selective_acks[i-1] = tp->selective_acks[i-1];
+				tp->selective_acks[i-1] = tp->selective_acks[i];
 			num_sacks--;
 			continue;
 		}
